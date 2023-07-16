6a5ab5662d03e868d812d98c2454788cb6b33218
sachin yadav <sachin.yadav@JIVOXLAPMAC0186.local>
2023-07-16 08:40:40 +0530

    Inherited Abstract class and defined its abstract function

---
+++ AbstractionDerivedCLass.java
@@ -0,0 +1,22 @@
+public class AbstractionDerivedCLass extends AbstractionImplementation{
+    @Override
+    public void eat(int quantity) {
+        System.out.printf("i am eating %d roti",quantity);
+    }
+    @Override
+    public void sleep(int hr) {
+        System.out.printf("i sleep %d hours",hr);
+    }
+    AbstractionDerivedCLass()
+    {
+        System.out.println("i am Derived class");
+    }
+    public static void main(String[] args)
+    {
+        AbstractionDerivedCLass obj=new AbstractionDerivedCLass();
+        obj.eat(5);
+        obj.fly();
+        obj.sleep(7);
+
+    }
+}
